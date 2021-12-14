fd8bb871703966a843c597e112554bb0bd9d8e24
JonasPalm <jonas.p.sorensen@gmail.com>
2021-12-14 17:55:28 +0100

    added chat room project to portfolio

---
+++ portfolio_projects/chat room project/scripts/ui.js
@@ -0,0 +1,30 @@
+// render chat templates to the dom
+// clear the list of chats (when the room changes)
+
+
+
+class ChatUI {
+    
+    constructor(list) {
+        this.list = list;
+    }
+    clear() {
+        this.list.innerHTML = "";
+    }
+    render(data) {
+        // format time using Fns DCN script
+        const when = dateFns.distanceInWordsToNow(
+            data.created_at.toDate(),
+            { addSuffix: true}
+        )
+        const html = `
+        <li class="list-group-item">
+        <span class="username">${data.username}</span>
+        <span class="message">${data.message}</span>
+        <div class="time">${when}</div>
+        </li>
+        `;
+
+        this.list.innerHTML += html;
+    }
+}
